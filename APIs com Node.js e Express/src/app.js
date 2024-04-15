f622a50a234b6109a5f1bb622db0458c7f9c80ba
Eric Sampaio <ericstudy97@gmail.com>
2024-04-15 09:50:47 -0300

    Continuando a construção da API com Express.

--- APIs com Node.js e Express/src/app.js
+++ APIs com Node.js e Express/src/app.js
@@ -2,8 +2,31 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+const livros = [
+    {
+        id: 0,
+        title: "Senhor dos Aneis"
+    },
+
+    {
+        id: 1,
+        title: "O Hobbit"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send('Hello World');
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
 });
 
 export default app;
