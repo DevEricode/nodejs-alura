27e9ee599ff2b8522c4590466d54a518ee7184d2
Eric Sampaio <ericstudy97@gmail.com>
2024-04-17 10:03:23 -0300

    Finalizando a segunda parte das aulas sobre crud.

--- APIs com Node.js e Express/src/app.js
+++ APIs com Node.js e Express/src/app.js
@@ -6,15 +6,21 @@ app.use(express.json());
 
 const livros = [
     {
-        id: 0,
+        id: 1,
         title: "Senhor dos Aneis"
     },
 
     {
-        id: 1,
+        id: 2,
         title: "O Hobbit"
     }
-]
+];
+
+function buscaLivro(id){
+    return livros.findIndex(livros => {
+        return livros.id === Number(id);
+    })
+}
 
 app.get("/", (req, res) => {
     res.status(200).send('Hello World');
@@ -24,9 +30,29 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].title = req.body.title;
+
+    res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+
+    res.status(201).send("Livro deletado com sucesso!");
+
 });
 
 export default app;
