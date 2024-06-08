0666d214736c13b11de47743e25ab2023a2107a3
Eric Sampaio <ericstudy97@gmail.com>
2024-06-08 10:56:58 -0300

    Validação do mongoose.

--- Node.js lidando com buscas, filtros, paginação e erros em uma API/src/models/Livro.js
+++ Node.js lidando com buscas, filtros, paginação e erros em uma API/src/models/Livro.js
@@ -2,11 +2,24 @@ import mongoose from 'mongoose';
 
 const livroSchema = new mongoose.Schema(
 	{
-		id: {type: String},
-		titulo: {type: String, required: true},
-		autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
-		editora: {type: String, required: true},
-		numeroPaginas: {type: Number},
+		id: { type: mongoose.Schema.Types.ObjectId },
+		titulo: {
+			type: String, 
+			required: [true, "O título do livro é obrigatório!"]
+		},
+		autor: {
+			type: mongoose.Schema.Types.ObjectId, 
+			ref: 'autores', 
+			required: [true, "O(A) autor(a) é obrigatório!"]
+		},
+		editora: {type: String, 
+			required: [true, "A editora é obrigatório!"]
+		},
+		numeroPaginas: {
+			type: Number,
+			min: [10, "O número de páginas permitido deve estar entre 10 e 5000. O valor digitado: {VALUE}."],
+			max: [5000, "O número de páginas permitido deve estar entre 10 e 5000. O valor digitado: {VALUE}."],
+		  },
 	},
 );
 
