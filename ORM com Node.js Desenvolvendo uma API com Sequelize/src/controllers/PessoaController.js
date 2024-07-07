5a9222824ff0257c6b6dd4a4c165c1d3c60fed79
Eric Sampaio <ericstudy97@gmail.com>
2024-07-07 15:13:47 -0300

    Criado as controllers e services.

--- ORM com Node.js Desenvolvendo uma API com Sequelize/src/controllers/PessoaController.js
+++ ORM com Node.js Desenvolvendo uma API com Sequelize/src/controllers/PessoaController.js
@@ -6,7 +6,7 @@ const pessoaServices = new PessoaServices();
 class PessoaController extends Controller {
     constructor() {
       super(pessoaServices);
-    }
-  }
+    };
+  };
   
   module.exports = PessoaController;
